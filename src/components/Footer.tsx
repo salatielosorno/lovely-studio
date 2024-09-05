fd6bed9605e106846fafaff85a5e33510c366bd2
salatielosorno <salatiel.linares@gmail.com>
2024-09-05 17:53:15 -0600

    feature(layout): add whatsapp link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,20 @@
 const Footer = () => (
-    <div className="md:container md:mx-auto text-center text-[12px] pt-10 pb-5">
-        <p>Todos los derechos reservados 2024 - Lovely Studio</p>
-    </div>
+    <>
+        <div className="chat-support__icons">
+            <a
+                className="chat-support__icon block mb-2"
+                href="https://api.whatsapp.com/send/?phone=5212251107907"
+                target="_blank"
+                rel="noreferrer"
+            >
+                <img src="/images/whats-icon.png" alt="whats-app-icon" />
+            </a>
+            {/* <div className="chat-support__icon"></div> */}
+        </div>
+        <div className="md:container md:mx-auto text-center text-[12px] pt-10 pb-5">
+            <p>Todos los derechos reservados 2024 - Lovely Studio</p>
+        </div>
+    </>
 )
 
 export { Footer }
